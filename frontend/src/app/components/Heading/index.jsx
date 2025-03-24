e00b1ea903d28267dd35f23f5eb6e669b86443a5
Alisher <alisherxujanov163@gmail.com>
2025-03-24 17:05:30 +0500

    update

--- frontend/src/app/components/Heading/index.jsx
+++ frontend/src/app/components/Heading/index.jsx
@@ -3,15 +3,9 @@ import "./style.scss"
 function Heading(props) {
     // props  ==>  properties  ==>  {children:null}
     const CHILDREN = props.children ? props.children : "Text is missing...!!!"
-
-    // JSX syntax => {...}
-    const STYLE = {
-        color: props.color,
-        backgroundColor: props.bg
-    }
     return (
-        <h1 className="heading-wrapper" style={STYLE}>
-            {CHILDREN}
+        <h1 className="heading-wrapper">
+            {CHILDREN} 
         </h1>
     )
 }
