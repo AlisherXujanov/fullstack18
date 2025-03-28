ef5318f888bb20913adf2e0e6dc6d315380f5a55
Alisher <alisherxujanov163@gmail.com>
2025-03-28 17:20:26 +0500

    update

--- frontend/src/app/components/Nav/index.jsx
+++ frontend/src/app/components/Nav/index.jsx
@@ -1,18 +1,31 @@
+"use client"
+
 import "./style.scss";
 import Image from "next/image"
 import LogoPng from "../../../assets/icons/logo.png"
 import Link from "next/link"
+import Searchbox from "../Searchbox"
+import { usePathname } from "next/navigation"
 
 // This is OLD version react.js
 // RU: это старый способ
-// import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom"
 // -----------------------------------------
 // This is NEW version from next.js
 // RU: это новый способ
-// import Link from "next/link";
+// import Link from "next/link"
+
+const navLinks = [
+    {  title: "Explore",  path: "/",  },
+    {  title: "About",    path: "/about" },
+    {  title: "Trending", path: "/trending" },
+    {  title: "FAQ",      path: "/faq" },
+]
 
 
 function Nav() {
+    const pathname = usePathname()
+
     return (
         <nav className="nav-wrapper">
             <div className="left">
@@ -23,14 +36,27 @@ function Nav() {
                         width={103} height={33}
                     />
                 </Link>
-                
+                <Searchbox />
             </div>
 
             <div className="right">
-                <Link href="/">Explore</Link>
+                {/* <Link href="/">Explore</Link>
                 <Link href="/about">About</Link>
                 <Link href="/trending">Trending</Link>
-                <Link href="/faq">FAQ</Link>
+                <Link href="/faq">FAQ</Link> */}
+                {
+                    navLinks.map((link) => {
+                        return (
+                            <Link 
+                                href={link.path} 
+                                key={link.title}
+                                className={pathname == link.path ? "active" : ""}
+                            >
+                                {link.title}
+                            </Link>
+                        )
+                    })
+                }
             </div>
         </nav>
     );
