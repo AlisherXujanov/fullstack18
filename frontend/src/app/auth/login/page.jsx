ac858631edab305efc3f22101bcd42b45c0fe32c
Alisher <alisherxujanov163@gmail.com>
2025-04-23 17:31:25 +0500

    created layout for login.jsx

--- frontend/src/app/auth/login/page.jsx
+++ frontend/src/app/auth/login/page.jsx
@@ -1,9 +1,47 @@
-import Link from "next/link";
+"use client"
+import { useState } from "react"
+import Link from "next/link"
+import "../style.scss"
 
 function Login() {
+
+    function submitForm(e) {
+        e.preventDefault()
+    }
+    function handleFormChange(e) {
+        console.log(e.target.value)
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="auth-container auth-signin-container">
+            <h1>Sign In</h1>
+            <form onSubmit={submitForm}>
+                <div className="form-field">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        onChange={handleFormChange}
+                        required
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleFormChange}
+                        required
+                    />
+                </div>
+                <div className="form-field">
+                    <button type="submit">Sign in</button>
+                </div>
+            </form>
 
             <p>
                 <small>
@@ -14,4 +52,4 @@ function Login() {
     )
 }
 
-export default Login;
+export default Login
