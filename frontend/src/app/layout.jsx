adbcf61b4caaa4a9de9a56e817048ad91c6f7b7b
Alisher <alisherxujanov163@gmail.com>
2025-05-05 17:29:54 +0500

    change store

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,18 +2,21 @@
 
 import "../assets/styles/initial.scss"
 import "../assets/styles/fouc-fix.css"
+import 'react-toastify/dist/ReactToastify.css'
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 import ThemeProvider from './providers/ThemeProvider'
 import { useAuth } from "@/hooks/useAuth"
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 import LoadingBar from './components/LoadingBar'
 import { useEffect, useState } from 'react'
+import { context, initialData } from '../store'
 
 export default function RootLayout({ children }) {
   const { user, loading } = useAuth()
   const [isReady, setIsReady] = useState(false)
+  const [store, setStore] = useState(initialData)
+  store.setStore = setStore
 
   useEffect(() => {
     setIsReady(true)
@@ -25,15 +28,17 @@ export default function RootLayout({ children }) {
         <title>Nft-Project</title>
       </head>
       <body>
-        <ThemeProvider>
-          <div className={`fouc-fix ${isReady ? 'ready' : ''}`}>
-            <LoadingBar />
-            <Nav user={user} />
-            {children}
-            <Footer />
-            <ToastContainer />
-          </div>
-        </ThemeProvider>
+        <context.Provider value={store}>
+          <ThemeProvider>
+            <div className={`fouc-fix ${isReady ? 'ready' : ''}`}>
+              <LoadingBar />
+              <Nav user={user} />
+              {children}
+              <Footer />
+              <ToastContainer />
+            </div>
+          </ThemeProvider>
+        </context.Provider>
       </body>
     </html>
   )
